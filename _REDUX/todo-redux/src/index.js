ca108e66596e99b3ea54fc49fe10d2549c3405b6
Corneliu <corneliu.gaina@gmail.com>
2019-08-20 14:19:28 +0200

    following redux documentation for a todo list : initialization

---
+++ _REDUX/todo-redux/src/index.js
@@ -0,0 +1,20 @@
+// entry point for Redux
+
+import React from 'react';
+import ReactDOM from 'react-dom';
+import './index.css';
+import App from './App';
+
+import { Provider } from 'react-redux';
+import { createStore } from 'redux';
+import rootReducer from './reducers/todos';
+
+
+const store = createStore(rootReducer)
+
+
+ReactDOM.render(
+    <Provider store={store}>
+        <App />
+    </Provider>, 
+    document.getElementById('root'));
